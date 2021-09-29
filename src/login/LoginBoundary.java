0c81ba8f9f09e7f2db6d084a5760c633e4bccba6
Diego Koga <60438897+DK-diegokoga@users.noreply.github.com>
2021-09-29 16:33:58 -0300

    Criado pasta Login

---
+++ src/login/LoginBoundary.java
@@ -0,0 +1,15 @@
+ package login;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginBoundary extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
